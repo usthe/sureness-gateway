432cbc7cfe780ddf83551881903b50118333382e
tomsun28 <tomsun28@outlook.com>
2021-04-28 22:06:16 +0800

    init spring-gateway

---
+++ src/test/java/com/usthe/sureness/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usthe.sureness.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
