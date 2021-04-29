608e4b03593a4f5a06d585eae6fd8d2e4aa7d35a
tomsun28 <tomsun28@outlook.com>
2021-04-29 23:32:01 +0800

    update use sureness 1.0.3, simplify the code

--- src/main/java/com/usthe/sureness/gateway/sureness/SurenessConfiguration.java
+++ src/main/java/com/usthe/sureness/gateway/sureness/SurenessConfiguration.java
@@ -1,30 +1,10 @@
 package com.usthe.sureness.gateway.sureness;
 
 
-import com.usthe.sureness.gateway.sureness.support.BasicSubjectReactiveCreator;
-import com.usthe.sureness.gateway.sureness.support.JwtSubjectReactiveCreator;
-import com.usthe.sureness.gateway.sureness.support.NoneSubjectReactiveCreator;
-import com.usthe.sureness.matcher.DefaultPathRoleMatcher;
-import com.usthe.sureness.matcher.PathTreeProvider;
-import com.usthe.sureness.mgt.SurenessSecurityManager;
-import com.usthe.sureness.processor.DefaultProcessorManager;
-import com.usthe.sureness.processor.Processor;
-import com.usthe.sureness.processor.support.JwtProcessor;
-import com.usthe.sureness.processor.support.NoneProcessor;
-import com.usthe.sureness.processor.support.PasswordProcessor;
-import com.usthe.sureness.provider.SurenessAccountProvider;
-import com.usthe.sureness.provider.ducument.DocumentAccountProvider;
-import com.usthe.sureness.provider.ducument.DocumentPathTreeProvider;
-import com.usthe.sureness.subject.SubjectCreate;
-import com.usthe.sureness.subject.SubjectFactory;
-import com.usthe.sureness.subject.SurenessSubjectFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.usthe.sureness.DefaultSurenessConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * sureness config
@@ -34,60 +14,13 @@ import java.util.List;
 @Configuration
 public class SurenessConfiguration {
 
-    private static final Logger logger = LoggerFactory.getLogger(SurenessConfiguration.class);
-
-    public SurenessConfiguration() {
-        init();
+    /**
+     * new sureness default config bean
+     * @return default config bean
+     */
+    @Bean
+    public DefaultSurenessConfig surenessConfig() {
+        return new DefaultSurenessConfig(DefaultSurenessConfig.SUPPORT_SPRING_REACTIVE);
     }
-
-    private void init() {
-
-        // process init
-        // account provider
-        SurenessAccountProvider accountProvider = new DocumentAccountProvider();
-        List<Processor> processorList = new LinkedList<>();
-        NoneProcessor noneProcessor = new NoneProcessor();
-        processorList.add(noneProcessor);
-        JwtProcessor jwtProcessor = new JwtProcessor();
-        processorList.add(jwtProcessor);
-        PasswordProcessor passwordProcessor = new PasswordProcessor();
-        passwordProcessor.setAccountProvider(accountProvider);
-        processorList.add(passwordProcessor);
-        DefaultProcessorManager processorManager = new DefaultProcessorManager(processorList);
-        if (logger.isDebugEnabled()) {
-            logger.debug("DefaultProcessorManager init");
-        }
-
-        // pathRoleMatcher init
-        // pathTree resource provider
-        PathTreeProvider pathTreeProvider = new DocumentPathTreeProvider();
-        DefaultPathRoleMatcher pathRoleMatcher = new DefaultPathRoleMatcher();
-        pathRoleMatcher.setPathTreeProvider(pathTreeProvider);
-        pathRoleMatcher.buildTree();
-        if (logger.isDebugEnabled()) {
-            logger.debug("DefaultPathRoleMatcher init");
-        }
-
-        // SubjectFactory init
-        SubjectFactory subjectFactory = new SurenessSubjectFactory();
-        List<SubjectCreate> subjectCreates = Arrays.asList(
-                new NoneSubjectReactiveCreator(),
-                new BasicSubjectReactiveCreator(),
-                new JwtSubjectReactiveCreator());
-        subjectFactory.registerSubjectCreator(subjectCreates);
-        if (logger.isDebugEnabled()) {
-            logger.debug("SurenessSubjectFactory init");
-        }
-
-        // surenessSecurityManager init
-        SurenessSecurityManager securityManager = SurenessSecurityManager.getInstance();
-        securityManager.setPathRoleMatcher(pathRoleMatcher);
-        securityManager.setSubjectFactory(subjectFactory);
-        securityManager.setProcessorManager(processorManager);
-        if (logger.isDebugEnabled()) {
-            logger.debug("SurenessSecurityManager init");
-        }
-    }
-
 
 }
--- src/main/java/com/usthe/sureness/gateway/sureness/SurenessFilter.java
+++ src/main/java/com/usthe/sureness/gateway/sureness/SurenessFilter.java
@@ -4,15 +4,23 @@ import com.usthe.sureness.mgt.SurenessSecurityManager;
 import com.usthe.sureness.processor.exception.*;
 import com.usthe.sureness.subject.SubjectSum;
 import com.usthe.sureness.util.SurenessContextHolder;
+import io.netty.buffer.UnpooledByteBufAllocator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.NettyDataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Map;
 
 /**
  * sureness filter
@@ -26,8 +34,6 @@ public class SurenessFilter implements GatewayFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request =  exchange.getRequest();
-        Integer statusCode = null;
-        String errorMsg = null;
         try {
             SubjectSum subject = SurenessSecurityManager.getInstance().checkIn(request);
             // You can consider using SurenessContextHolder to bind subject in threadLocal
@@ -35,40 +41,55 @@ public class SurenessFilter implements GatewayFilter, Ordered {
             if (subject != null) {
                 SurenessContextHolder.bindSubject(subject);
             }
-        } catch (ProcessorNotFoundException | UnknownAccountException | UnsupportedSubjectException e4) {
+        } catch (IncorrectCredentialsException | UnknownAccountException | ExpiredCredentialsException e1) {
             logger.debug("this request is illegal");
-            statusCode = HttpStatus.BAD_REQUEST.value();
-            errorMsg = e4.getMessage();
+            return responseWrite(exchange, HttpStatus.UNAUTHORIZED, e1.getMessage(),null);
         } catch (DisabledAccountException | ExcessiveAttemptsException e2 ) {
             logger.debug("the account is disabled");
-            statusCode = HttpStatus.FORBIDDEN.value();
-            errorMsg = e2.getMessage();
-        } catch (IncorrectCredentialsException | ExpiredCredentialsException e3) {
-            logger.debug("this account credential is incorrect or expired");
-            statusCode = HttpStatus.FORBIDDEN.value();
-            errorMsg = e3.getMessage();
+            return responseWrite(exchange, HttpStatus.UNAUTHORIZED, e2.getMessage(),null);
+        } catch (NeedDigestInfoException e5) {
+            logger.debug("you should try once again with digest auth information");
+            return responseWrite(exchange, HttpStatus.UNAUTHORIZED,
+                    "try once again with digest auth information",
+                    Collections.singletonMap("WWW-Authenticate", e5.getAuthenticate()));
         } catch (UnauthorizedException e5) {
             logger.debug("this account can not access this resource");
-            statusCode = HttpStatus.FORBIDDEN.value();
-            errorMsg = e5.getMessage();
+            return responseWrite(exchange, HttpStatus.FORBIDDEN, e5.getMessage(),null);
         } catch (RuntimeException e) {
             logger.error("other exception happen: ", e);
-            statusCode = HttpStatus.FORBIDDEN.value();
-            errorMsg = e.getMessage();
+            return responseWrite(exchange, HttpStatus.FORBIDDEN, e.getMessage(),null);
         }
 
-        // auth error filter to error collect api
-        if (statusCode != null && errorMsg != null) {
-            exchange.getResponse().setRawStatusCode(statusCode);
-            exchange.getResponse().getHeaders().add("errorMsg", errorMsg);
-            return exchange.getResponse().setComplete();
-        } else {
-            return chain.filter(exchange).doFinally(x -> SurenessContextHolder.unbindSubject());
-        }
+        return chain.filter(exchange).doFinally(x -> SurenessContextHolder.unbindSubject());
     }
 
     @Override
     public int getOrder() {
         return 0;
+    }
+
+    /**
+     * write response json data
+     * @param exchange content
+     * @param statusCode statusCode
+     * @param message message
+     */
+    private Mono<Void> responseWrite(ServerWebExchange exchange, HttpStatus statusCode,
+                               String message, Map<String,String> headers) {
+
+        exchange.getResponse().setStatusCode(statusCode);
+        if (headers != null) {
+            headers.forEach((key, value) -> exchange.getResponse().getHeaders().add(key, value));
+        }
+        if (message != null) {
+            return exchange.getResponse().writeWith(Flux.create(sink -> {
+                        NettyDataBufferFactory nettyDataBufferFactory = new NettyDataBufferFactory(new UnpooledByteBufAllocator(false));
+                        DataBuffer dataBuffer= nettyDataBufferFactory.wrap(message.getBytes(StandardCharsets.UTF_8));
+                        sink.next(dataBuffer);
+                        sink.complete();
+                    }));
+        } else {
+            return exchange.getResponse().setComplete();
+        }
     }
 }
